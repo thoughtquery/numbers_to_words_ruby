2b3fbcee95dc9dfdde8978e98ed789bc1a03f48c
Jeff Stringer & Andrew M Benavides <student@epicodus.com>
2014-07-24 11:53:10 -0700

    develop specs and code for numbers less than 100

--- lib/numberstowords.rb
+++ lib/numberstowords.rb
@@ -2,8 +2,18 @@ def number_to_words(num)
   words = []
   onesAndTweens = { 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen" }
   tens = { 2 => "twenty", 3 => "thirty", 4 => "forty", 5 => "fifty", 6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety" }
-
-
+  len = num.to_s.length
+  onesPlace = num.to_s.slice(-1).to_i
+  tensPlace = num.to_s.slice(-2).to_i
+  if num==0
+    words << 'zero'
+  elsif num>0 && num<20
+    words << onesAndTweens[num]
+  else
+    words.push(tens[tensPlace],onesAndTweens[onesPlace])
+  end
+  words.join(" ")
+  #words.push(onesAndTweens[onesPlace.to_i])
 end
 
-puts number_to_words(10)
+puts number_to_words(25)
--- spec/numberstowords_spec.rb
+++ spec/numberstowords_spec.rb
@@ -1,8 +1,20 @@
 require ('rspec')
 require ('numberstowords')
 
-describe('number_to_words')  do
+describe('number_to_words') do
   it("converts the number 0 to 'zero'") do
-    expect(number_to_words(0).to eq("zero")
+    expect(number_to_words(0)).to (eq("zero"))
+  end
+
+  it("better convert 5 to 'five") do
+    expect(number_to_words(5)).to eq("five")
+  end
+
+  it("better convert 25 to 'twenty five") do
+    expect(number_to_words(25)).to eq("twenty five")
+  end
+
+  it("better convert 25 to 'twenty five") do
+    expect(number_to_words(99)).to eq("ninety nine")
   end
 end
